cb3a6d27609f8afc04fbca4ae2811deeed1256c4
nzengi <zenginureddin1@gmail.com>
2024-08-17 15:35:45 +0300

    Create app.js

---
+++ Frontend/app.js
@@ -0,0 +1,5 @@
+// Basit frontend için Web3 entegrasyonu
+const Web3 = require("web3");
+const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+
+// Akıllı sözleşme adresleri ve ABI'lerle etkileşim sağlanacak...
